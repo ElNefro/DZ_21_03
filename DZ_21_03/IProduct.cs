8cc51a722e376ba2e2cb5e6849b9557aa0785a45
ElNefro <yanik0206@gmail.com>
2025-04-15 00:09:28 +0500

    Добавьте файлы проекта.

---
+++ DZ_21_03/IProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_21_03
+{
+    public interface IProduct
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        decimal Price { get; set; }
+        string Category { get; set; }
+        int Quantity { get; set; }
+    }
+}
